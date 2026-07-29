f44b8af48fb4ca8892024dd99b6baab85ed87d57
hossammax005 <hossammax05@gmail.com>
2026-07-29 10:35:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-Initial HEVAL STAGE v0.1
+const enterButton = document.getElementById("enterButton");
+
+enterButton.addEventListener("click", () => {
+
+    enterButton.innerHTML = "Loading...";
+
+    enterButton.disabled = true;
+
+    enterButton.style.opacity = "0.7";
+
+    document.body.style.transition = "1s";
+
+    document.body.style.filter = "brightness(1.25)";
+
+    setTimeout(() => {
+
+        alert("Welcome to HEVAL STAGE");
+
+        document.body.style.filter = "brightness(1)";
+
+        enterButton.innerHTML = "ENTER";
+
+        enterButton.disabled = false;
+
+        enterButton.style.opacity = "1";
+
+    },1200);
+
+});
