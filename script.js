4d83826c37c303bcbe208a79cd8488417eda7995
hossammax005 <hossammax05@gmail.com>
2026-07-29 11:58:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,27 @@
 const enterButton = document.getElementById("enterButton");
+const container = document.querySelector(".container");
+const stars = document.getElementById("stars");
 
 enterButton.addEventListener("click", () => {
 
+    enterButton.disabled = true;
     enterButton.innerHTML = "ENTERING...";
 
-    enterButton.disabled = true;
+    stars.style.transition = "2s";
+    stars.style.transform = "scale(1.4)";
+    stars.style.opacity = "0.9";
 
-    document.body.style.transition = "1s";
+    container.style.transition = "1.8s";
+    container.style.transform = "scale(.92)";
+    container.style.opacity = "0";
 
-    document.body.style.transform = "scale(1.05)";
-
-    document.body.style.filter = "brightness(1.3)";
-
-    setTimeout(() => {
-
-        document.body.style.opacity = "0";
-
-    },800);
+    document.body.style.transition = "2s";
+    document.body.style.background =
+        "radial-gradient(circle,#53d8ff 0%,#102970 30%,#02030a 100%)";
 
     setTimeout(() => {
 
-        alert("Welcome to HEVAL");
-
-        document.body.style.opacity = "1";
-        document.body.style.transform = "scale(1)";
-        document.body.style.filter = "brightness(1)";
-        enterButton.innerHTML = "ENTER";
-        enterButton.disabled = false;
+        window.location.href = "portal.html";
 
     },1800);
 
